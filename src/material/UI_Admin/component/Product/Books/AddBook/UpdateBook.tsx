a63873103d6bd4678593c5ee867b978ad1746dfb
ciscopilux <tranquocb306@gmail.com>
2022-09-06 22:12:45 +0700

    delete ck

--- src/material/UI_Admin/component/Product/Books/AddBook/UpdateBook.tsx
+++ src/material/UI_Admin/component/Product/Books/AddBook/UpdateBook.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 import './UpdateBook.css'
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import {
     TextField
     , Autocomplete
